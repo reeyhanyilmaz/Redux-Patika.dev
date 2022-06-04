9d56bfba42b0a37803c5fb5286c10b020abf67bc
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-04 14:43:56 +0300

    add pending and rejected

--- breaking-bad-app/src/redux/charactersSlice.js
+++ breaking-bad-app/src/redux/charactersSlice.js
@@ -1,29 +1,42 @@
-import axios from 'axios';
-import {createSlice ,createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-
-const char_limit= 12;
-export const fetchCharacters = createAsyncThunk("characters/getCharacters" , async () => {
-    const res = await axios(`${process.env.REACT_APP_API_ENDPOINT}/characters?limit=${char_limit}`);
+const char_limit = 12;
+export const fetchCharacters = createAsyncThunk(
+  "characters/getCharacters",
+  async () => {
+    const res = await axios(
+      `${process.env.REACT_APP_API_ENDPOINT}/characters?limit=${char_limit}`
+    );
     return res.data;
-})
+  }
+);
 
-export const charactersSlice = createSlice({ //bunu store.js' dahil edicez.
-    name: "characters",//dosyanın ismi.
-    initialState: {
-        items: [],
+export const charactersSlice = createSlice({
+  //bunu store.js' dahil edicez.
+  name: "characters", //dosyanın ismi.
+  initialState: {
+    items: [],
+    isLoading: false,
+  },
+  reducers: {},
+
+  //async actionları buraya yazıyoruz.
+  extraReducers: {
+    [fetchCharacters.pending]: (state, action) => {
+      state.isLoading = true;
     },
-    reducers: {},
-
-    //async actionları buraya yazıyoruz.
-    extraReducers: {
-        [fetchCharacters.fulfilled]: (state, action) => {
-            state.items = action.payload;
-            console.log("action.payload", action.payload); 
-            //action.payload bizim objelerimiz oldu (karakter ve bilgilerini içeren).
-        }
-
-    }
-}) 
+    [fetchCharacters.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.isLoading = false;
+      console.log("action.payload", action.payload);
+      //action.payload bizim objelerimiz oldu (karakter ve bilgilerini içeren).
+    },
+    [fetchCharacters.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    },
+  },
+});
 
 export default charactersSlice.reducer;
