5e63884151b2c9689f6cdb71028b94828cc32552
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-04 12:09:09 +0300

    add react-router

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+    </ul>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+  </div>
   );
+}
+
+function Home() {
+  return <div>Home</div>
+}
+
+function About() {
+  return <div>About</div>
 }
 
 export default App;
