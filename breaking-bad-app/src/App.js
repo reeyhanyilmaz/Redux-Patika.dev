791c94a5da8e96b11b5c7a5444ccdf7b531cdd52
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-06 10:43:56 +0300

    add navbar and quotes folder

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -2,13 +2,26 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Quotes from "./pages/Quotes";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Characters</Link>
+          </li>
+          <li>
+            <Link to="/quotes">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/char/:char_id" element={<Detail />} />
+        <Route path="/quotes" element={<Quotes />} />
       </Routes>
     </div>
   );
