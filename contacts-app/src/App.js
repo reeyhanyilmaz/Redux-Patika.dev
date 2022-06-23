78043efca2f39de1e7e61f0f6e64846259009b47
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-23 17:58:29 +0300

    add Error page

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Contacts from './components/Contacts';
 import Edit from './components/Contacts/Edit';
+import Error from './components/Error';
 import { Routes, Route} from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Contacts />  } />
           <Route path="/edit/:id" element={<Edit />} />
+          <Route path="*" element={<Error  />} />
       </Routes> 
       </div>            
     </div>
---
+++ contacts-app/src/components/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>This page was not found</div>
+  )
+}
+
+export default Error;  
