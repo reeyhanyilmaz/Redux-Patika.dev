29bae048d33c4b361ab86f8c71349672d0969ad5
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-23 17:24:52 +0300

    add finish project

--- contacts-app/src/components/Contacts/Edit.js
+++ contacts-app/src/components/Contacts/Edit.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useParams} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 import EditForm from './EditForm';
 import {useSelector} from "react-redux";
 import {contactSelector} from "../../redux/contactSlice";
@@ -7,8 +7,14 @@ import {contactSelector} from "../../redux/contactSlice";
 function Edit() {
   const {id} = useParams(); //parametreleri alıyoruz.
   console.log('id', id);
+  const navigate = useNavigate();
   const contact = useSelector(state => contactSelector.selectById(state, id))
   console.log('contact :>> ', contact); //id'ye ail eleman gelir.
+
+  if(!contact){
+    return navigate("/"); 
+  }
+
  return (
     <div>
       <h1>Edit</h1>
