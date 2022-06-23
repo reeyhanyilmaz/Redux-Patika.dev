6585931722de27c679ba51500f69b284ed1ce3db
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-23 13:42:56 +0300

    add selectTotal (total list number)

--- contacts-app/src/components/Contacts/index.js
+++ contacts-app/src/components/Contacts/index.js
@@ -1,10 +1,14 @@
 import Form from "./Form";
 import List from './List';
+import {useSelector} from "react-redux";
+import {contactSelector} from "../../redux/contactSlice";
 
 function Contacts() {
+
+  const total = useSelector(contactSelector.selectTotal); //contactSelector state.contacts getirir bize.
   return (
     <div id="container">
-        <h1>Contacts</h1>
+        <h1>Contacts ({total}) </h1>
         <Form />
         <List /> 
     </div>
