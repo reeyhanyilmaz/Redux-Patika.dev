570f0654a9260c0bf3ff393c3ad7b40e497af986
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-23 14:01:19 +0300

    add routes

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Contacts from './components/Contacts';
+import Edit from './components/Edit';
+import { Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Contacts />  
+        <Routes>
+          <Route path="/" element={<Contacts />  } />
+          <Route path="/edit/:id" element={<Edit />} />
+      </Routes>      
     </div>
   );
 }
